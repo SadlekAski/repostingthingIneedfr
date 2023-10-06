822b73d2328707c531480ff08a55be306ff7218c
SadlekAski <129619951+SadlekAski@users.noreply.github.com>
2023-10-06 22:49:19 +0800

    Update FluentDocsIg.lua

--- FluentTutorialfrfrfr/FluentDocsIg.lua
+++ FluentTutorialfrfrfr/FluentDocsIg.lua
@@ -73,6 +73,20 @@ do
     Options.MyToggle:SetValue(false)
 
 
+    local Toggle2 = Tabs.Main:AddToggle("MyToggle", {Title = "+", Default = false })
+
+    Toggle:OnChanged(function(arg)
+       if arg then
+        local dothing = "dothing"
+            rint(dothing)
+        else
+            local notdothing = "Not doing thing"
+            print(notdothing)
+        end
+    end)
+
+    Options.MyToggle:SetValue(false)
+
     
     local Slider = Tabs.Main:AddSlider("Slider", {
         Title = "Slider",
