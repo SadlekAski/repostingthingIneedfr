0dcab6fb9d711f37b737a313dd1ef0f2bc46ca7f
SadlekAski <129619951+SadlekAski@users.noreply.github.com>
2023-10-08 01:58:59 +0800

    Create toggle.lua

---
+++ FluentTutorialfrfrfr/toggle.lua
@@ -0,0 +1,10 @@
+--much better toggle frfrfrvdfbgfbfgbcvnhghfjftyum
+
+Main:AddToggle("IdkName", {
+        Title = "Name",
+        Description = "Makes Something",
+        Default = false,
+        Callback = function(Value)
+            -- do something
+        end
+    })
